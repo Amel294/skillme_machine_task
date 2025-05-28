baf0be29e5b9738826561205391e6ca10fd6fb01
Amel Umesh <45140781+Amel294@users.noreply.github.com>
2025-05-29 01:23:26 +0530

    fix : responsiveness of services section

--- src/components/services/Services.tsx
+++ src/components/services/Services.tsx
@@ -13,19 +13,18 @@ const services = [
   { name: "PC Game Development", icon: ComputerDesktopIcon },
   { name: "PS4 Game Development", icon: PuzzlePieceIcon },
   { name: "AR/VR Solutions", icon: CubeTransparentIcon },
-  { name: "AR/VR design", icon: CameraIcon },
+  { name: "AR/VR Design", icon: CameraIcon },
   { name: "3D Modelings", icon: CubeIcon },
 ];
 
 export default function Services() {
   return (
     <section
-      className="relative text-white py-16  md:px-20 bg-cover bg-center bg-no-repeat min-h-screen flex items-center justify-center"
+      className="relative text-white py-16 md:px-20 bg-cover bg-center bg-no-repeat min-h-screen flex items-center justify-center"
       style={{ backgroundImage: `url(${serviceBg})` }}
     >
       <div className="absolute inset-0 bg-black opacity-80"></div>
 
-      {/* Centered content */}
       <div className="relative z-10 max-w-screen-xl w-full">
         <div className="text-center mb-10 px-4">
           <h2 className="text-3xl font-bold mb-4">
@@ -39,9 +38,12 @@ export default function Services() {
           </p>
         </div>
 
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 text-center px-4 w-full">
-          {services.slice(0, 4).map(({ name, icon: Icon }) => (
-            <div key={name} className="space-y-3">
+        <div className="flex flex-wrap justify-center gap-8 px-4">
+          {services.map(({ name, icon: Icon }) => (
+            <div
+              key={name}
+              className="w-full sm:w-[45%] md:w-[22%] space-y-3 text-center"
+            >
               <div className="w-14 h-14 bg-white rounded-full flex items-center justify-center mx-auto">
                 <Icon className="w-6 h-6 text-orange-500" />
               </div>
@@ -49,17 +51,6 @@ export default function Services() {
               <div className="text-orange-500 text-lg">→</div>
             </div>
           ))}
-          <div className="md:flex gap-8 md:w-full justify-center md:col-span-4">
-            {services.slice(4).map(({ name, icon: Icon }) => (
-              <div key={name} className="space-y-3 mb-10">
-                <div className="w-14 h-14 bg-white rounded-full flex items-center justify-center mx-auto">
-                  <Icon className="w-6 h-6 text-orange-500 " />
-                </div>
-                <p className="text-white font-medium">{name}</p>
-                <div className="text-orange-500 text-lg">→</div>
-              </div>
-            ))}
-          </div>
         </div>
       </div>
     </section>
